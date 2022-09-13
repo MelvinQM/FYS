d7a434fc3527a39c6e350ee10142a7e451d8f300
Melvin Moes <melvin.moes@hva.nl>
2022-09-13 09:28:47 +0200

    Update TestGit.java

--- TestGit.java
+++ TestGit.java
@@ -2,5 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         String myString = "Fout";
         System.out.println(myString);
+        String jeMama = "Je mama YUUGE";
+        System.out.ptintln(jeMama);
     }
 }

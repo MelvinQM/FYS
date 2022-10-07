0094d173f9809824feb9f535651118bb10d42a3e
Koen Lammers <koen.lammers@hva.nl>
2022-10-07 13:54:03 +0200

    Update servo/servo.py

--- servo/servo.py
+++ servo/servo.py
@@ -12,17 +12,21 @@ servoPin = 1
 wpi.pinMode(servoPin, wpi.PWM_OUTPUT)
 
 # variabelen in steller voor het bewegen van de servo
-delayTime = 0.1
+delayTime = 0.3
 minMove = 70
-maxMove = 470
+maxMove = 460
+
+# 70 is 0 graden
+# 265 is 90 graden
+# 469 is 180 graden
+# 1 graden is 2.94 move
 
 # loop met het bewegen van de servo
 try:
     while True:
-        move = random.randint(minMove, maxMove)
+        move =  random.randint(minMove, maxMove)
         wpi.pwmWrite(servoPin, move)
         time.sleep(delayTime)
-        move = random.randint(minMove, maxMove)
         wpi.pwmWrite(servoPin, move)
         time.sleep(delayTime)
 
@@ -30,4 +34,4 @@ try:
 except KeyboardInterrupt:
     pass
 # zorgen dat de servo weer naar het 0 punt beweegt
-wpi.pwmWrite(servoPin, maxMove)
+wpi.pwmWrite(servoPin, minMove)

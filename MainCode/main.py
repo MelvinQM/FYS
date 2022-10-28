47a0cbe82396a3bf6b4e99dd9bfb54d1a006a680
zweerss <simon.zweers@hva.nl>
2022-10-28 12:08:23 +0200

    Remote coding sessie en main.py geedit met Melvin, Koen, Simon

--- MainCode/main.py
+++ MainCode/main.py
@@ -3,17 +3,17 @@
 ## @author Koen, Melvin, Simon, Jayden
 import random
 import time
+import threading
+import logging
 import odroid_wiringpi as wpi
-
-# Zorgen dat de wpi pins worden gebruikt
-wpi.wiringPiSetup()
 
 # Servo pin aanwijzen en instellen
 servoPin = 1
+servoDelay = 0.5
 wpi.pinMode(servoPin, wpi.PWM_OUTPUT)
 
 # De pin van de sound sensor
-soundSensor_PIN = 25
+soundSensor_PIN = 37
 LED_PIN = 9
 wpi.pinMode(LED_PIN, wpi.OUTPUT)
 
@@ -22,25 +22,63 @@ oldSound = 1460
 minMove = 0
 maxMove = 180
 
-# Loop die zorgt voor functionaliteit servo en soundsensor
-while True:
-    # user input beweging optie
-    # angle = float(input('Enter angle between 0 & 180: '))
-    # move = ((angle/18)+2)*45
-    move = random.randint(int((minMove/18)+2)*45, int((maxMove/18)+2)*45)
-    wpi.pwmWrite(servoPin, int(move))
-    time.sleep(0.5)
-    try:
+# Zorgen dat de wpi pins worden gebruikt
+wpi.wiringPiSetup()
+
+# Function for usage of Sound Sensor
+def soundSensor():
+    while True:
         # analogRead leest een float value van de sensor af (Geluid dus)
         sound = wpi.analogRead(soundSensor_PIN)
         print(sound)
         # Vergelijk het gelezen value met een preset value die je kan instellen bij oldSound
         if sound > oldSound:
             wpi.digitalWrite(LED_PIN, wpi.HIGH)
+            print("wpi.HIGH")
         else:
             wpi.digitalWrite(LED_PIN, wpi.LOW)
+            print("wpi.LOW")
+
+
+
+def servoMovement():
+    while True:
+        try:
+            # user input beweging optie
+            # angle = float(input('Enter angle between 0 & 180: '))
+            # move = ((angle/18)+2)*45
+            move = random.randint(int((minMove / 18) + 2) * 45, int((maxMove / 18) + 2) * 45)
+            wpi.pwmWrite(servoPin, int(move))
+            time.sleep(servoDelay)
+
+        except KeyboardInterrupt:
+            wpi.digitalWrite(LED_PIN, wpi.LOW)
+            print("Measurement stopped by User")
+
+
+# Thread aangemaakt
+soundThread = threading.Thread(target=soundSensor)
+servoThread = threading.Thread(target=servoMovement)
+# Start threading
+soundThread.start()
+servoThread.start()
+
+
+
+""" 
+# Loop die zorgt voor functionaliteit servo en soundsensor
+while True:
+    try:
+        # user input beweging optie
+        # angle = float(input('Enter angle between 0 & 180: '))
+        # move = ((angle/18)+2)*45
+        move = random.randint(int((minMove / 18) + 2) * 45, int((maxMove / 18) + 2) * 45)
+        wpi.pwmWrite(servoPin, int(move))
+        time.sleep(0.5)
 
     # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         wpi.digitalWrite(LED_PIN, wpi.LOW)
         print("Measurement stopped by User")
+"""
+

a797215e68c0c6421707f965dc99eeb0abbed608
jayden <jayden2004@engineer.com>
2022-11-04 11:33:09 +0100

    Laser toegevoegd aan LDR code

--- ldr_code/ldr_counter.py
+++ ldr_code/ldr_counter.py
@@ -1,11 +1,23 @@
-import time
 import odroid_wiringpi as wpi
+import time
 
-LDR_PIN = 3
-LDR_DELAY = 0.1
 
+LDR_PIN = 9
+# + op pin 1 (3.3v)
+# - op pin 5 (ground)
+# S op pin 6 (ALT1 output)
+LASER_PIN = 12
+
+LDR_DELAY = 0.05
+
+wpi.wiringPiSetup()
+
+wpi.pinMode(LASER_PIN, wpi.OUTPUT)
 wpi.pinMode(LDR_PIN, wpi.INPUT)
+output = 0
 outputOld = 0
+
+wpi.digitalWrite(LASER_PIN, wpi.HIGH)
 
 while True:
     output = wpi.digitalRead(LDR_PIN)
@@ -13,6 +25,6 @@ while True:
         print("Lichtje Uit")
     elif output < outputOld:
         print("Lichtje Aan")
-    print(output)
+    # print(output)
     outputOld = output
     time.sleep(LDR_DELAY)

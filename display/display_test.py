46c345027f010bec481ca1f7183f9dcfc4ba1c9b
jayden <jayden2004@engineer.com>
2022-11-15 10:56:30 +0100

    pins voor display aangewezen en display getest

---
+++ display/display_test.py
@@ -0,0 +1,10 @@
+import odroid_wiringpi as wpi
+import time
+
+wpi.WiringPiSetup()
+
+#GPIO pins voor de segments en digits
+#segments =  (11,4,23,8,7,10,18,25)
+#digits = (22,27,17,24)
+
+wpi.pinMode

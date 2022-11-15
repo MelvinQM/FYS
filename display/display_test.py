8dc6cca3867fb30f154fcc92dde3cd49efd09771
jayden <jayden2004@engineer.com>
2022-11-15 11:44:32 +0100

    alle lichtjes gaan aan

--- display/display_test.py
+++ display/display_test.py
@@ -1,10 +1,12 @@
 import odroid_wiringpi as wpi
 import time
 
-wpi.WiringPiSetup()
+wpi.wiringPiSetup()
+# physical pins 7, 11, 12, 13, 15, 16, 26, 29
+SEGMENT_PINS = (7, 0, 1, 2, 3, 4, 11, 21)
+# TEST_PIN = 7
 
-#GPIO pins voor de segments en digits
-#segments =  (11,4,23,8,7,10,18,25)
-#digits = (22,27,17,24)
 
-wpi.pinMode
+for segment in SEGMENT_PINS:
+    wpi.pinMode(segment, wpi.OUTPUT)
+    wpi.digitalWrite(segment, wpi.LOW)

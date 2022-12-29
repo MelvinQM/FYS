428947de6352e4556518c12bc433e299cbec603d
moesmq <melvin.moes@hva.nl>
2022-12-29 15:46:29 +0000

    Changed some code to work more efficiently with testing

--- servo/servo.py
+++ servo/servo.py
@@ -1,37 +1,34 @@
+import odroid_wiringpi as wpi
 import random
 import time
 
-import odroid_wiringpi as wpi
-from time import sleep
-
-# zorgen dat de wpi pins worden gebruikt
-wpi.wiringPiSetup()
-
-# servo pin aanwijzen en instellen
+# Setup
 servoPin = 1
+
+wpi.wiringPiSetup()
 wpi.pinMode(servoPin, wpi.PWM_OUTPUT)
 
-# variabelen in steller voor het bewegen van de servo
-delayTime = 0.3
-minMove = 70
-maxMove = 460
+gameCountdown = 120
+servoDelay = 0.5
+# Servo min en max
+minMove = 0
+maxMove = 180
+resetMove = 315
 
-# 70 is 0 graden
-# 265 is 90 graden
-# 469 is 180 graden
-# 1 graden is 2.94 move
 
-# loop met het bewegen van de servo
-try:
-    while True:
-        move =  random.randint(minMove, maxMove)
+def servomovement():
+    global gameCountdown
+    oldMove = 90
+    killTimer = gameCountdown
+    # Start program at 90 degrees
+    wpi.pwmWrite(servoPin, resetMove)
+    time.sleep(1)
+    while killTimer > 0:
+        move = random.randint(int((minMove / 18) + 2) * 45, int((maxMove / 18) + 2) * 45)
         wpi.pwmWrite(servoPin, move)
-        time.sleep(delayTime)
-        wpi.pwmWrite(servoPin, move)
-        time.sleep(delayTime)
-
-# zorgen dat de loop stopt bij 'ctrl c'
-except KeyboardInterrupt:
-    pass
-# zorgen dat de servo weer naar het 0 punt beweegt
-wpi.pwmWrite(servoPin, minMove)
+        time.sleep(servoDelay)
+        print(move)
+        killTimer -= 0.5
+    # End program on 90
+    wpi.pwmWrite(servoPin, resetMove)
+servomovement()

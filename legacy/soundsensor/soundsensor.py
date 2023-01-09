f42e18c94d43657f7994c26207001dca65f144f6
Jayden van Oorschot <jayden.van.oorschot@hva.nl>
2023-01-09 15:59:54 +0100

    prints voor geluidsniveau en comments toegevoegd

--- legacy/soundsensor/soundsensor.py
+++ legacy/soundsensor/soundsensor.py
@@ -7,9 +7,18 @@ wpi.wiringPiSetup()
 SOUND_PIN = 29
 soundLevel = 0
 
-# wpi.pinMode(SOUND_PIN, wpi.INPUT)
-
 while 1:
-    print(wpi.analogRead(SOUND_PIN))
-    time.sleep(0.1)
+    #leest analoge waarde van sound sensor
+    soundLevel = wpi.analogRead(SOUND_PIN)
 
+    #print algemeen geluidsniveau
+    if soundLevel > 1500:
+        print("Er is heel veel geluid!")
+    elif soundLevel > 1300:
+        print("Er is best veel geluid.")
+    elif soundLevel > 1200:
+        print("Er is wat geluid.")
+    else:
+        print("het is stil.")
+
+    time.sleep(0.01)

f275720e6a2675d529081146fc7a93b79dac85f2
Jayden van Oorschot <jayden.van.oorschot@hva.nl>
2023-01-09 15:07:40 +0100

    soundsensor print analoge waarde

--- legacy/soundsensor/soundsensor.py
+++ legacy/soundsensor/soundsensor.py
@@ -1,29 +1,15 @@
 import odroid_wiringpi as wpi
 import time
 
-# De pin van de sound sensor
-soundSensor_PIN = 25
-LED_PIN = 9
 wpi.wiringPiSetup()
-#wpi.pinMode(soundSensor_PIN, wpi.INPUT)
-wpi.pinMode(LED_PIN, wpi.OUTPUT)
 
-oldSound = 1460
-while True:
-    time.sleep(0.01)
-    try:
-        sound = wpi.analogRead(soundSensor_PIN)
-        print(sound)
-        if sound > oldSound:
-            wpi.digitalWrite(LED_PIN, wpi.HIGH)
-            #print("Aan")
-        else:
-            wpi.digitalWrite(LED_PIN, wpi.LOW)
-            #print("Uit")
+# fysieke pin 40
+SOUND_PIN = 29
+soundLevel = 0
 
-    # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        wpi.digitalWrite(LED_PIN, wpi.LOW)
-        print("Measurement stopped by User")
+# wpi.pinMode(SOUND_PIN, wpi.INPUT)
 
+while 1:
+    print(wpi.analogRead(SOUND_PIN))
+    time.sleep(0.1)
 

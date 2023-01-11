a52cfb03f6629a099c94e368d4040272c1f15472
Melvin Moes <melvin.moes@hva.nl>
2023-01-11 16:20:06 +0000

    added segmentdisplay.

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -65,6 +65,8 @@ def startgame():
         countdownThread.start()
     if servoThread.is_alive() == False:
         servoThread.start()
+    if segmentThread.is_alive() == False:
+        segmentThread.start()
 
     return render_template("game.html", name_user=name_user)
 
@@ -261,22 +263,66 @@ def neopixelUltra():
             print("Measured Distance = %.1f cm" % dist)
 
 
-# def neopixelUltra():
-#     if __name__ == '__main__':
-#         while True:
-#             global dist
-#             # dist is a variable made for distance()
-#             dist = distance
-#             # if statement that tells if distance is smaller than 100cm lights turn on
-#             if dist <= 100:
-#                 ws.write2812(spi, stoplichtBlauw)
-#                 time.sleep(0.1)
-#                 ws.write2812(spi, stoplichtWit)
-#                 time.sleep(0.1)
-#                 ws.write2812(spi, stoplichtRood)
-#             # else statements that tells if distance is larger than 100 cm light turn off
-#             else:
-#                 ws.write2812(spi, stoplichtWit)
+def segmentDisplay():
+    # fysieke pins: 13, 15, (36), 16, 29, 31, 33, 35
+    SEGMENT_PINS = (2, 3, 27, 4, 21, 22, 23, 24)
+    # fysieke pins 18, 22, 26, 32
+    DIGIT_PINS = (5, 6, 11, 26)
+
+    # setup voor de pins van de segmenten
+    for segment in SEGMENT_PINS:
+        wpi.pinMode(segment, wpi.OUTPUT)
+        wpi.digitalWrite(segment, 0)
+
+    # setup voor de pins van de 4 digits
+    for digit in DIGIT_PINS:
+        wpi.pinMode(digit, wpi.OUTPUT)
+        wpi.digitalWrite(digit, 1)
+
+    # geeft aan welke pins aan gaan voor elk getal
+    getalArray = {' ': (0, 0, 0, 0, 0, 0, 0, 0),
+                  '0': (1, 1, 0, 1, 1, 1, 1, 0),
+                  '1': (0, 0, 0, 1, 0, 1, 0, 0),
+                  '2': (1, 1, 0, 0, 1, 1, 0, 1),
+                  '3': (0, 1, 0, 1, 1, 1, 0, 1),
+                  '4': (0, 0, 0, 1, 1, 1, 1, 0),
+                  '5': (0, 1, 0, 1, 1, 0, 1, 1),
+                  '6': (1, 1, 0, 1, 1, 0, 1, 1),
+                  '7': (0, 0, 0, 1, 0, 1, 0, 1),
+                  '8': (1, 1, 0, 1, 1, 1, 1, 1),
+                  '9': (0, 1, 0, 1, 1, 1, 1, 1)}
+
+    # het getal dat op de display moet staan, wordt ook een string zodat elk getal kan worden opgezocht in de array
+
+
+    # Setup for the segments
+    for segment in SEGMENT_PINS:
+        wpi.pinMode(segment, wpi.OUTPUT)
+        wpi.digitalWrite(segment, 0)
+
+    # Setup for the 4 digits
+    for digit in DIGIT_PINS:
+        wpi.pinMode(digit, wpi.OUTPUT)
+        wpi.digitalWrite(digit, 1)
+    # het getal dat op de display moet staan, wordt ook een string zodat elk getal kan worden opgezocht in de array
+    getal = 1234
+    getalString = str(getal).rjust(4)
+
+    #try:
+        # de loop die de juiste pins aan zet voor elk getal op de display
+    while True:
+        getal = score * 100
+        getalString = str(getal).rjust(4)
+        for digit in range(4):
+            for loop in range(0, 8):
+                wpi.digitalWrite(SEGMENT_PINS[loop], getalArray[getalString[digit]][loop])
+            # zet de juiste digit aan voor 0.001 seconde, zodat op elke digit een ander getal kan staan
+            wpi.digitalWrite(DIGIT_PINS[digit], 0)
+            time.sleep(0.001)
+            wpi.digitalWrite(DIGIT_PINS[digit], 1)
+
+    #except KeyboardInterrupt:
+        #wpi.ditigalWrite(SEGMENT_PINS, wpi.LOW)
 
 
 def databaseInsert():
@@ -315,12 +361,12 @@ ultraSonicThread = threading.Thread(target=ultrasonic)
 insertThread = threading.Thread(target=databaseInsert)
 readThread = threading.Thread(target=databaseRead)
 neopixelThread = threading.Thread(target=neopixelUltra)
-
+segmentThread = threading.Thread(target=segmentDisplay)
 
 if __name__ == '__main__':
     soundThread.start()
     ultraSonicThread.start()
-    ldrThread.start()
+    # ldrThread.start()
     insertThread.start()
     readThread.start()
     neopixelThread.start()
